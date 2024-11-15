8c735283eaf0747b24a2adf4159b7686d9b2afcb
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-11-15 15:54:52 -0700

    feat: Add API_KEY to env for use
    Add .env file to gitignore. Import API_KEY using vite's syntax and
    ensure it works

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import sampleSize from "lodash.samplesize";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export function Game() {
   const storedBestScore = JSON.parse(localStorage.getItem("best-score"));
@@ -18,38 +21,32 @@ export function Game() {
   const [selectedCards, setSelectedCards] = useState([]);
   const [currentCards, setCurrentCards] = useState([]);
 
-  // useEffect to increase score after each round
-  useEffect(() => {
-    if (!gameOver) {
-      setScore((s) => s + 1);
-    }
-  }, [selectedCards, gameOver]);
-
-
-  //   useEffect(() => {
-  if (score > bestScore) {
-    setBestScore(score);
-  }
-  //   }, [score, bestScore]);
-  
-
-  // Latest bestScore gets saved in localStorage
+  // Latest bestScore gets saved in localStorage every time it changes
   useEffect(() => {
     localStorage.setItem("best-score", JSON.stringify(bestScore));
   }, [bestScore]);
 
+  // useEffect to fetch from the API
+  useEffect(() => {}, [selectedCards]);
+
   // Function to run when the user clicks on the card
   function handleCardClick(cardId) {
-    if(selectedCards.includes(cardId)){
-        setGameOver(true);
+    if (selectedCards.includes(cardId)) {
+      setGameOver(true);
     } else {
-        selectedCards.push(cardId);
+      setSelectedCards([...selectedCards, cardId]);
+      setScore((s) => s + 1);
+
+      // See if this works, if it updates one lesser than score instead
+      // of with score, then make sure to add this logic to a useEffect instead
+      if (score > bestScore) {
+        setBestScore(score);
+      }
     }
   }
 
+  // Function to get random cards from the deck
   function getRandomCards(deck) {
-    // Logic to shuffle the deck
+    return sampleSize(deck, 12);
   }
-
-
 }
